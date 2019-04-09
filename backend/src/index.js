987ce1eb8a34791c11033b3c3e90eb07210f9f43
Jhow <jhonata.miranda@tatix.com.br>
2019-04-09 16:30:18 -0300

    [Backend] - Criação de Estrutura da api e websocket

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,27 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.send('Hello World');
-});
+// Iniciando o DB
+mongoose.connect(
+	"mongodb://localhost:27017/goweek",
+	{ useNewUrlParser: true}
+);
 
-app.listen(3000, () => {
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+app.use( (req, res, next) => {
+	req.io = io;
+
+	return next();
+})
+
+app.use(cors());
+app.use(express.json());
+app.use(require('./routes'));
+
+server.listen(3000, () => {
     console.log('Server started on port 3000');
 });
-

de17b344258aa9030673d802bfa8360426f453d7
Jhow <jhonata.miranda@tatix.com.br>
2019-04-09 20:45:17 -0300

    Parte Web da aplicação com ReactJS

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -8,6 +8,6 @@ const LikeController = require('./controllers/LikeController');
 routes.get('/tweets', TweetController.index);
 routes.post('/tweets', TweetController.store);
 
-routes.get('/likes/:id', LikeController.store);
+routes.post('/likes/:id', LikeController.store);
 
 module.exports = routes;
